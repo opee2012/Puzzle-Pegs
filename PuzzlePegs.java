23eb0e211dd525908b9143dd9a38c74fd8067193
opee2012 <jborth2010@gmail.com>
2022-12-02 16:49:45 -0500

    Project Complete

--- PuzzlePegs.java
+++ PuzzlePegs.java
@@ -1,10 +1,12 @@
 // In collaboration with Hannah, Mei-Rose, and Phoenix.
 
-public class PuzzlePegs {
-    private static final char peg = 'P'; // Represents Pegs (Done) '\u25CF'
-    private static final char hole = 'H'; // Represents Holes (Done) '\u25CB'
+import java.util.*;
 
-    /* Done */
+
+public class PuzzlePegs {
+    private static final char peg = '\u25CF'; // Represents Pegs
+    private static final char hole = '\u25CB'; // Represents Holes
+
     // 2D array representing all possible moves
     private static final int[][] moves = {
             { 1, 2, 4 },
@@ -45,65 +47,67 @@ public class PuzzlePegs {
             { 15, 14, 13 }
     };
 
-    // Board History (Linked List?)
+    // Board History (Array list)
+    private final List<char[]> boardHist;
 
 
-    // Jumps History (Linked List?)
-
+    // Jumps History (Array list)
+    private final List<String> jumpHist;
 
     // Starting hole location
-    int startLoc = 1;
+    int startLoc;
 
     // Ending hole location - (optional)
+    int endLoc;
+
+    /**
+     * Constructor
+     *
+     * @param boardHist The starting location of the hole
+     * @param jumpHist The ending location of a peg
+     */
+    public PuzzlePegs(List<char[]> boardHist, List<String> jumpHist) {
+        this.boardHist = boardHist;
+        this.jumpHist = jumpHist;
+    }
 
     /**
      * Constructor
      *
      * @param startLoc The starting location of the hole
+     * @param endLoc The ending location of a peg
      */
-    PuzzlePegs(int startLoc) throws IllegalArgumentException {
+    PuzzlePegs(int startLoc, int endLoc) throws IllegalArgumentException {
         // Assign starting hole location
-        if (!maintainBounds(startLoc)) {
+        if (maintainBounds(startLoc)) {
             throw new IllegalArgumentException("Invalid starting hole, must be range from 1 to 15.");
         } else {
             this.startLoc = startLoc;
         }
 
-        // Initiate board and jump history
+        if ((maintainBounds(endLoc)) && (endLoc != -1)) {
+            throw new IllegalArgumentException("Ending location must range between 1 and 15.");
+        } else {
+            this.endLoc = endLoc;
+        }
+
+        boardHist = new ArrayList<>();
+        jumpHist = new ArrayList<>();
     }
 
     /**
      * Checks if argument value is between 1 and 15
-     *
-     * @param argValue
      */
     private static boolean maintainBounds(int argValue) {
-        if ((argValue >= 1) && (argValue <= 15)) {
-            return true;
-        } else {
-            return false;
-        }
+        return (argValue <= 0) || (argValue > 15);
     }
 
     public void solve() {
         // build board
-
-        // solve puzzle
-        // print the moves and output
-    }
-
-    private void solveRecur(char[] board) {
-        // go through every possible move
-        // match PPH pattern
-        // record board history
-
-
-    }
-
-    /* public char[] buildBoard() {
         char[] board = new char[16];
         board[0] = ' '; // Unused Null Space
-        for (var i = 1; i < board.length; i++) {
+
+        for (int i = 1; i < board.length; i++) {
             if (startLoc == i) {
                 board[i] = hole;
             } else {
@@ -111,10 +115,70 @@ public class PuzzlePegs {
             }
         }
 
-        return board;
-    } */
+        // store original board
+        var original = board.clone();
 
-    /* Done */
+        // solve puzzle
+        if (solveRecur(board)) {
+            System.out.println("Initial Board");
+            printBoard(original);
+
+            int boardHistSize = boardHist.size();
+
+            // Print boards for solved & the board history
+            for (int i = 1; i < boardHistSize; i++) {
+                // Print jump history
+                System.out.println(jumpHist.get(boardHistSize - i));
+                printBoard(boardHist.get(i - 1));
+            }
+            System.out.println(jumpHist.get(0));
+            printBoard(boardHist.get(boardHistSize - 1));
+        } else {
+            System.out.println("No solution can be found.");
+        }
+    }
+
+    /**
+     * Recursive function making use of backtracking
+     */
+    private boolean solveRecur(char[] board) {
+        // go through every possible jump
+        for (var jump : moves) {
+            // match PPH pattern
+            if ((board[jump[0]] == peg) && (board[jump[1]] == peg) && (board[jump[2]] == hole)) {
+                // make the jump
+                board[jump[0]] = hole;
+                board[jump[1]] = hole;
+                board[jump[2]] = peg;
+
+                // record new board in history
+                var clonedBoard = board.clone();
+                boardHist.add(clonedBoard);
+
+                if (solveRecur(board)) {
+                    // record jump
+                    jumpHist.add("\nMoved " + jump[0] + " to " + jump[2] + ", jumping over " + jump[1]);
+                    return true;
+                }
+
+                boardHist.remove(boardHist.size() - 1);
+                board[jump[0]] = peg;
+                board[jump[1]] = peg;
+                board[jump[2]] = hole;
+            }
+        }
+
+        int count = 0;
+        for (var c : board) {
+            if (c == peg) count++;
+        }
+
+        if ((count == 1) && (endLoc == -1)) return true;
+        else return (count == 1) && (board[endLoc] == peg);
+
+
+    }
+
     private static void printBoard(char[] Board) {
         System.out.println("    " + Board[1]);
         System.out.println("   " + Board[2] + " " + Board[3]);
@@ -123,10 +187,15 @@ public class PuzzlePegs {
         System.out.println(Board[11] + " " + Board[12] + " " + Board[13] + " " + Board[14] + " " + Board[15]);
     }
 
-
     public static void main(String[] args) {
 
-        printBoard(buildBoard());
+        int startPeg = Integer.parseInt(args[0]);
+        int endPeg;
+
+        if (args.length == 1) endPeg = -1;
+        else endPeg = Integer.parseInt(args[1]);
+
+        PuzzlePegs puzzle = new PuzzlePegs(startPeg, endPeg);
+        puzzle.solve();
     }
 }
-
